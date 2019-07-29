f55978ad97c6c8ef88fc6f7b1831e1fac05e48ed
Ashna Shah-Grover <ashnashahgrover777@gmail.com>
2019-07-29 17:10:12 -0400

    Login CLI

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -3,4 +3,43 @@ class User <ActiveRecord::Base
 has_many :responses
 has_many :hacks, through: :responses
 
+  def self.login
+
+    username = nil
+    while !username
+      puts "Please enter username:"
+      username = gets.chomp
+      username = User.find_by("username = ?", username)
+      if username == nil
+        puts "Username not found. Please try again."
+      end
+    end
+
+    password = nil
+    while !password
+      puts "Please enter password"
+      password = gets.chomp
+      if password == username.password
+        return username
+        binding.pry
+      else
+        password == nil
+        "Wrong Password. Please try again."
+      end
+    end
+
+  end
+
+  def self.signup
+    puts "Please enter a username"
+    username = gets.chomp
+    if User.find_by("username = ?", username)
+      puts "Username already exists. Please try again."
+      self.signup
+    end
+    puts "Please enter a password"
+    password = gets.chomp
+    self.create(username:username, password:password)
+  end
+
 end
