ba187c1007fc5b675895064365f2869282b53da3
Ashna Shah-Grover <ashnashahgrover777@gmail.com>
2019-07-31 12:55:49 -0400

    Basic CRUD Model

--- lib/models/response.rb
+++ lib/models/response.rb
@@ -4,19 +4,19 @@ class Response <ActiveRecord::Base
     belongs_to :hack
 
     #returns an array of hack ids for the user
-    def find_by_likes(user_id_arg)
-        user_responses = select_response_by_user(user_id_arg)
-        return user_responses.map do |response|
-            response.hack_id
-        end.uniq
-    end
-
-    def self.display_my_likes(user_id_arg)
-        my_likes = select_response_by_user(user_id_arg)
-        my_likes.each do |like|
-            Hack.display_hack(like.hack_id)
-        end
-    end
+    # def find_by_likes(user_id_arg)
+    #     user_responses = select_response_by_user(user_id_arg)
+    #     return user_responses.map do |response|
+    #         response.hack_id
+    #     end.uniq
+    # end
+    #
+    # def self.display_my_likes(user_id_arg)
+    #     my_likes = select_response_by_user(user_id_arg)
+    #     my_likes.each do |like|
+    #         Hack.display_hack(like.hack_id)
+    #     end
+    # end
 
     def self.view_comments(hack)
       responses = self.all.where("hack_id = ?", hack.id)
